6343c474dd000fd87a75732cb9f671a7eb12c5d8
Artur-Poffo <arturpoffoc@hotmail.com>
2022-09-08 13:31:37 -0300

    corrigindo deploy

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -5,7 +5,7 @@ export function Cards(props) {
   return (
     <div onClick={() => props.handleFlipped(props.card)} className={`card ${props.card.flipped ? "flip" : ""}`} id={props.card.id} >
       <div className="card_front">
-        <img src={`./assets/${props.card.icon}.png`} alt={props.card.icon} />
+        <img src={`./src/imgs/${props.card.icon}.png`} alt={props.card.icon} />
       </div>
 
       <div className="card_back">
